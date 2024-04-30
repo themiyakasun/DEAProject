b47f64b9c0a2209f41fd1a34cfc1c9a2e3ff9aff
kasunharitha <kasunharitha2020@gmail.com>
2024-04-30 22:52:25 +0530

    merge conflicts

--- src/java/myPackage/cart/ContactInformationServlet.java
+++ src/java/myPackage/cart/ContactInformationServlet.java
@@ -2,7 +2,6 @@ package myPackage.cart;
 
 import com.google.gson.Gson;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
