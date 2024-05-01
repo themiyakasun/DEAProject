458d9a1c7cfbba3978b90a93bd614f5d1a337393
themiyakasun <themiyakasun.tb@gmail.com>
2024-05-01 11:11:04 +0530

    Admin fetch category

--- web/assets/js/admin.js
+++ web/assets/js/admin.js
@@ -12,3 +12,46 @@ function addCategory(){
     });
     return false;
 }
+
+
+$(document).ready(function() {
+    //Display Categories
+    function fetchCategories() {
+        $.ajax({
+        url: contextPath + '/CategoriesServlet',
+        type: 'GET',
+        dataType: 'json',
+        success: function(categories) {
+            var tbody = $('#categoriesTableBody');
+            var dropdown = $('#pro_cat');
+            tbody.empty(); 
+            
+            if (categories.length === 0) {
+                tbody.append('<tr><td colspan="5" class="text-center">No categories available</td></tr>');
+            } else {
+                $.each(categories, function(index, category) {
+                    var row = $('<tr>').addClass('category-row').attr('data-cat-id', category.catId);
+                    row.append($('<td>').addClass('cat-id').text(category.catId));
+                    row.append($('<td>').addClass('cat-name').text(category.catName));
+                    row.append($('<td>').addClass('cat-slug').text(category.catSlug));
+                    row.append($('<td>').addClass('cat-order').text(category.catOrder));
+                    row.append($('<td>').addClass('action').html(
+                        '<div class="d-flex align-items-center gap-3 fs-6">' +
+                        '<button class="text-warning edit-info" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Edit info" aria-label="Edit" onclick="editCategory(' + category.catId + ')"><img src="../assets/icons/edit.png"/></button>' +
+                        '<button class="text-danger delete-category" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Delete" aria-label="Delete" onclick="deleteCategory(' + category.catId + ')"><img src="../assets/icons/trash.png"/></button>' +
+                        '</div>'
+                    ));
+                    tbody.append(row);
+                    
+                    dropdown.append($('<option>').text(category.catName).val(category.catId));
+                });
+            }
+        },
+        error: function() {
+            alert('Error fetching categories.');
+        }
+        });
+    }
+    
+    fetchCategories();
+});
