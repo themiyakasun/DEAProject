3beaa857306b1323f6d118d6ff8f6aac33d259d5
themiyakasun <themiyakasun.tb@gmail.com>
2024-05-01 11:04:31 +0530

    Add category

--- web/assets/js/admin.js
+++ web/assets/js/admin.js
@@ -1,1 +1,14 @@
-
+//Add Category
+function addCategory(){
+    var formData = $("#categoryForm").serialize();
+    $.ajax({
+        type: "POST",
+        url: contextPath + "/AddCategoryServlet",
+        data: formData,
+        success: function(response) {
+            $("#loadingIndicator").hide();
+            alert(response);
+        }
+    });
+    return false;
+}
--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -486,3 +486,5 @@ function sendCheckoutData() {
     },
   });
 }
+
+
