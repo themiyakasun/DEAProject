a390739637c474c3833ad4cf8788c1bbaebb8fa3
kgssandeepa <kgssandeepa@students.nsbm.ac.lk>
2024-05-09 23:04:10 +0530

    Get total products

--- src/java/myPackage/admin/GetTotalsServlet.java
+++ src/java/myPackage/admin/GetTotalsServlet.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package myPackage.admin;
 
 import java.io.IOException;
@@ -12,75 +7,60 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author Heezi
- */
 public class GetTotalsServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 3.2 Final//EN\">");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet GetTotalsServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet GetTotalsServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+    private static class TotalInfo {
+        int totalProducts;
+        int totalCategories;
+        int totalUsers;
+        int totalOrders;
+
+        TotalInfo(int totalProducts, int totalCategories, int totalUsers, int totalOrders) {
+            this.totalProducts = totalProducts;
+            this.totalCategories = totalCategories;
+            this.totalUsers = totalUsers;
+            this.totalOrders = totalOrders;
         }
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        
+        int totalProducts = getTotalProducts();
+        int totalCategories = getTotalCategories();
+        int totalUsers = getTotalUsers();
+        int totalOrders = getTotalOrders();
+        
+        TotalInfo totalInfo = new TotalInfo(totalProducts, totalCategories, totalUsers, totalOrders);
+        
+        String json = new Gson().toJson(totalInfo);
+        
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        
+        response.getWriter().write(json);
+
     }
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    private int getTotalProducts(){
+        try(Connection conn = DbUtil.getConnection()){
+            String query = "SELECT COUNT(*) FROM products";
+            
+            try(PreparedStatement statement = conn.prepareStatement(query)){
+               
+                try(ResultSet result = statement.executeQuery()){
+                    if(result.next()){
+                        int count = result.getInt(1);
+                        return count;
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.getMessage();
+        }
+         return -1;
     }
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
