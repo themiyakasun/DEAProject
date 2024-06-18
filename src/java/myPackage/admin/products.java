8e1b910b9c19f14e7cd4995f00586601d20f6e92
M-Ajmal <mrlocal62363@gmail.com>
2024-06-19 00:00:06 +0530

    #

---
+++ src/java/myPackage/admin/products.java
@@ -0,0 +1,12 @@
+package myPackage.admin;
+
+public class products {
+    int proId;
+    int catId;
+    String proName;
+    double proPrice;
+    String proImg;
+    String proDesc;
+    double proReviews;
+    String date;
+}
