b42e35269a7ba0350d03934e1c5f6a240f0c92d3
M-Ajmal <mrlocal62363@gmail.com>
2024-06-19 00:00:55 +0530

    #

--- src/java/myPackage/admin/products.java
+++ src/java/myPackage/admin/products.java
@@ -9,4 +9,15 @@ public class products {
     String proDesc;
     double proReviews;
     String date;
+
+    public Product(int proId, int catId, String proName, double proPrice, String proImg, String proDesc, double proReviews, String date){
+        this.proId = proId;
+        this.catId = catId;
+        this.proName = proName;
+        this.proPrice = proPrice;
+        this.proImg = proImg;
+        this.proDesc = proDesc;
+        this.proReviews = proReviews;
+        this.date = date;
+    }
 }
