039689cb4f22aa329e89e4f0442ff1a31b1b9bd2
M-Ajmal <mrlocal62363@gmail.com>
2024-06-19 00:41:54 +0530

    #

--- src/java/myPackage/admin/getpro.java
+++ src/java/myPackage/admin/getpro.java
@@ -1,4 +1,5 @@
 package myPackage.admin;
+
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -12,10 +13,11 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import myPackage.db.DbUtil;
 
 public class getpro {
-
+  //  connection to the database
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
